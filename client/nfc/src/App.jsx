afc0b9e40974d90dec299dfad75ed3bb97c3a731
AKSHAT CONTRACTOR <akshatcontractor@gmail.com>
2024-08-30 00:29:27 +0530

    Some changes and made specs and description compoponent

--- client/nfc/src/App.jsx
+++ client/nfc/src/App.jsx
@@ -7,11 +7,11 @@ import Signup from '../src/pages/SignUp.jsx';
 import SignIn from './pages/SignIn.jsx';
 import CartPage from './pages/Cart.jsx';
 import Home from './pages/Home.jsx';
-import Dash from './pages/Dashboard.jsx'
-import Analytics from "./pages/Analytics.jsx";
 import Settings from './pages/Settings.jsx';
 import Products from "./pages/Products.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
+import ProductInformation from "./components/ProdInformation.jsx";
+
 
 
 function App() {
@@ -22,20 +22,23 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />}>
-            <Route path="/landingpage" element={<LandingPage/>}/>
-            <Route path="/dashboard" element={<LandingPage/>} />
-            <Route path="/analytics" element={<LandingPage/>} />
+            <Route path="/landingpage" element={<LandingPage />} />
+            <Route path="/dashboard" element={<LandingPage />} />
+            <Route path="/analytics" element={<LandingPage />} />
             <Route path="/products" element={<Products />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/signIn" element={<SignIn />} />
             <Route path="/cartpage" element={<CartPage />} />
+            <Route path="/prodinfo" element={<ProductInformation />} />
           </Route>
-
-
-
-
         </Routes>
+
+
+
+
+
+
       </Router>
 
     </>
